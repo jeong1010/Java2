e0e17ac375598fd8e534968d4db50b35009d147b
jeongmin.hwang <margdol20@gmail.com>
2024-03-21 14:42:01 +0900

    while 반복문을 통해 0부터 9까지 출력하는 프로그램.
    ps. 무한루프에 빠졌으면 콘솔창 오른쪽 상단에 정지 버튼 클릭.

---
+++ src/LoopEx1.java
@@ -0,0 +1,16 @@
+
+public class LoopEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 0부터 9까지 출력하기
+		int index = 0;
+		
+		while(index <= 9) {
+			System.out.print(index + " ");
+			//index = index + 1;
+			index++;
+		}
+	}
+
+}
