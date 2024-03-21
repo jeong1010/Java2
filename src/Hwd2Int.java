429679f6961af9849e2fd009474956bc8490e915
jeongmin.hwang <margdol20@gmail.com>
2024-03-21 14:35:37 +0900

    switch-case문에서 case가 여러 개인 경우 쓸 수 있는 문법을 다양하게 수정함.

--- src/Hwd2Int.java
+++ src/Hwd2Int.java
@@ -11,7 +11,8 @@ public class Hwd2Int {
 		Hwdnumber = input.next(); // 기본적으로 문자로 입력받아서 next 뒤에 뭐 안붙음.
 		
 		switch(Hwdnumber) {
-		case "하나","일","첫째":
+		case "하나": // 이런식으로도 쓸 수 있다. (break 없이)
+		case "일","첫째":
 			System.out.println(1);
 			break;
 		case "둘","이","둘째":
