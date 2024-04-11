7f5fed7870bac1dd0d625ecf3c23aa023277c174
jeongmin.hwang <margdol20@gmail.com>
2024-04-11 13:06:17 +0900

    Television 생성자, 생성자 오버로딩관련 실습내용 추가. tv1.channel = ~~~ 이런식으로 쓰는 게 아닌 tv1을 선언할 때 파라미터로 c, v, o 값을 보내주도록 함.

--- src/Television.java
+++ src/Television.java
@@ -1,8 +1,33 @@
 
 public class Television {
-	private int channel = 7; // 채널에 대한 권한을 내부에서만.
-	int volume;
-	boolean onOff;
+	private int channel = 7; // 채널에 대한 권한을 내부에서만. / 채널을 7로 초기화.
+	private int volume;
+	private boolean onOff;
+	
+	public Television() { // 생성자 오버로딩. 
+		this(1,1,false); // 자기 함수를 다시 호출. 
+		//channel = 1; // 값 지정이 오버로딩에서 여러 번 들어가면? 형식상 어긋난 것임
+		//volume = 1;
+		//onOff = false;
+	}
+	
+	public Television(int c) {
+		this(c,1,false); // 이런식으로 c만 받을 수도 있다.
+	}
+	
+	public Television(int c, int v) { // 같은 이름, 같은 매개변수, 같은 반환인자면 오버로딩이 안됨.
+		// 이런식으로 매개변수 달리하면 허용이 됨.
+		this(c,v,false); // 이런식으로 c만 받을 수도 있다.
+	}
+	
+	// 생성자 추가. int c, int v, boolean o)
+	public Television(int c, int v, boolean o) { // 생성자 오버로딩을 하면 결국 매개변수 가장 많은 한 쪽으로 모이기 때문에 한 곳에서 처리를 하게 됨.
+		this.channel = c;
+		this.volume = v;
+		onOff = o;
+	}
+	
+	
 
 	void print() {
 		System.out.println("텔레비전의 채널은 " + channel + "이고 볼륨은 " + volume + "입니다.");
--- src/TelevisionTest.java
+++ src/TelevisionTest.java
@@ -4,12 +4,12 @@ public class TelevisionTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Television tv1; // tv1은 참조변수. 가리키는 대상이 Television.
-		tv1 = new Television(); // Television 공간을 새로 만들어준 뒤 인스턴스를 만들어 지칭.
+		tv1 = new Television(7,10,true); // Television 공간을 새로 만들어준 뒤 인스턴스를 만들어 지칭.
 		//tv1.channel = 7; // 값을 저장하는 게 아닌, 값이 있는 위치를 알려주는 것.
-		tv1.setChannel(3);
-		tv1.setChannel(15); // 3 나와야 함.
-		tv1.volume = 10;
-		tv1.onOff = true;
+		//tv1.setChannel(3);
+		//tv1.setChannel(15); // 3 나와야 함.
+		//tv1.volume = 10;
+		//tv1.onOff = true;
 		tv1.print();
 		//System.out.println("텔레비전의 채널은 " + tv1.channel + "이고 볼륨은 " + tv1.volume + "입니다.");
 		
@@ -21,8 +21,8 @@ public class TelevisionTest {
 		//tv2 = tv1;
 		//tv2.channel = 6;
 		//tv2.setChannel(6); // 값을 직접 바꾸는 게 아닌, 함수를 통해 수정.
-		tv1.setChannel(-1); // 7 나와야 함.
-		tv2.volume = 12; // tv2 = tv1라고 했으면 값이 아니라 위치를 가리켰기 때문에 tv1도 바뀐 것처럼 나올 것임.
+		//tv1.setChannel(-1); // 7 나와야 함.
+		//tv2.volume = 12; // tv2 = tv1라고 했으면 값이 아니라 위치를 가리켰기 때문에 tv1도 바뀐 것처럼 나올 것임.
 						// tv1과 관련 없이 만드려면 새로운 객체로 만들어줘야 함.
 		//System.out.println("텔레비전의 채널은 " + tv2.channel + "이고 볼륨은 " + tv2.volume + "입니다.");
 		tv2.print();
